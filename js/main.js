ba9e6311a8180226ff3982f59489bb6993fea3f8
Josh <pana23@gmail.com>
2020-02-25 11:42:56 +1100

    adding the flip function to the cards

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // There is only 8 way a player can win
-// (1-2-3) (4,5,6) (7,8,9) (1,4,7) ....
+// (0-1-2) (3,4,5) (6,7,8) (0,3,6) (1,4,7) (2,5,8) (0,4,8) (2,4,6) ....
 // Create a series of if statements that test if the current player has won
 // Player 1 starts picks a sqaure red
 // Player 2 starts with blue
@@ -54,126 +54,40 @@ for (let i = 0; i < 9; i++) {
 	}
 
 	// Check if the player has won
+	// There is only 8 way a player can win
+	// (0-1-2) (3,4,5) (6,7,8) (0,3,6) (1,4,7) (2,5,8) (0,4,8) (2,4,6)
 	const isGameOver = function () {
-		if (boxValues[0] === boxValues[1] && boxValues[2] === boxValues[1]) {
-			alert("You have won the game");
+		if (boxValues[0] === boxValues[1] && boxValues[2] === boxValues[1] & boxValues[0] !== "") {
 			console.log("You have won");
+			$(".game-status").html("<h4>You have won the game</h4>");
+		} else if(boxValues[3] === boxValues[4] && boxValues[5] === boxValues[3] & boxValues[3] !== "") {
+			console.log("You have won");
+			$(".game-status").html("<h4>You have won the game</h4>");
+		} else if (boxValues[6] === boxValues[7] && boxValues[8] === boxValues[6] & boxValues[6] !== "") {
+			console.log("You have won");
+			$(".game-status").html("<h4>You have won the game</h4>");
+		} else if (boxValues[0] === boxValues[3] && boxValues[6] === boxValues[0] & boxValues[0] !== "") {
+			console.log("You have won");
+			$(".game-status").html("<h4>You have won the game</h4>");
+		} else if (boxValues[1] === boxValues[4] && boxValues[7] === boxValues[1] & boxValues[1] !== "") {
+			console.log("You have won");
+			$(".game-status").html("<h4>You have won the game</h4>");
+		} else if (boxValues[2] === boxValues[5] && boxValues[8] === boxValues[2] & boxValues[2] !== "") {
+			console.log("You have won");
+			$(".game-status").html("<h4>You have won the game</h4>");
+		} else if (boxValues[0] === boxValues[4] && boxValues[8] === boxValues[0] & boxValues[0] !== "") {
+			console.log("You have won");
+			$(".game-status").html("<h4>You have won the game</h4>");
+		} else if (boxValues[2] === boxValues[4] && boxValues[6] === boxValues[2] & boxValues[2] !== "") {
+			console.log("You have won");
+			$(".game-status").html("<h4>You have won the game</h4>");
 		} else {
-			console.log("Hey mate");
+			console.log("Play again");
 		}
 	}
 
 
-// $('#0').on('click', function() {
-//   boxValues[0] = currentPlayer;
-// 	if (currentPlayer === true) {
-// 		$(this).addClass("active-red");
-// 	} else {
-// 		$(this).addClass("active-blue");
-// 	}
-//   currentPlayer = !currentPlayer;
-// });
-//
-// $('#1').on('click', function() {
-//   boxValues[1] = currentPlayer;
-// 	if (currentPlayer === true) {
-// 		$(this).addClass("active-red");
-// 	} else {
-// 		$(this).addClass("active-blue");
-// 	}
-//   currentPlayer = !currentPlayer;
-// });
-//
-// $('#2').on('click', function() {
-//   boxValues[2] = currentPlayer;
-// 	if (currentPlayer === true) {
-// 		$(this).addClass("active-red");
-// 	} else {
-// 		$(this).addClass("active-blue");
-// 	}
-//   currentPlayer = !currentPlayer;
-// });
-//
-// $('#3').on('click', function() {
-//   boxValues[3] = currentPlayer;
-// 	if (currentPlayer === true) {
-// 		$(this).addClass("active-red");
-// 	} else {
-// 		$(this).addClass("active-blue");
-// 	}
-//   currentPlayer = !currentPlayer;
-// });
-//
-// $('#4').on('click', function() {
-//   boxValues[4] = currentPlayer;
-// 	if (currentPlayer === true) {
-// 		$(this).addClass("active-red");
-// 	} else {
-// 		$(this).addClass("active-blue");
-// 	}
-//   currentPlayer = !currentPlayer;
-// });
-//
-// $('#5').on('click', function() {
-//   boxValues[5] = currentPlayer;
-// 	if (currentPlayer === true) {
-// 		$(this).addClass("active-red");
-// 	} else {
-// 		$(this).addClass("active-blue");
-// 	}
-//   currentPlayer = !currentPlayer;
-// });
-//
-// $('#6').on('click', function() {
-//   boxValues[6] = currentPlayer;
-// 	if (currentPlayer === true) {
-// 		$(this).addClass("active-red");
-// 	} else {
-// 		$(this).addClass("active-blue");
-// 	}
-//   currentPlayer = !currentPlayer;
-// });
-//
-// $('#7').on('click', function() {
-//   boxValues[7] = currentPlayer;
-// 	if (currentPlayer === true) {
-// 		$(this).addClass("active-red");
-// 	} else {
-// 		$(this).addClass("active-blue");
-// 	}
-//   currentPlayer = !currentPlayer;
-// });
-//
-// $('#8').on('click', function() {
-//   boxValues[8] = currentPlayer;
-// 	if (currentPlayer === true) {
-// 		$(this).addClass("active-red");
-// 	} else {
-// 		$(this).addClass("active-blue");
-// 	}
-//   currentPlayer = !currentPlayer;
-// });
-
-// Create loop that finds if each of the boxes equals a win
-
-// const game = function() {
-// 	/// ??
-// 	console.log(wins[2]);
-// };
-
-// $("#0").on('click', function() {
-// 	$(this).addClass("active-red");
-// 	// boxValues[0] =
-// });
-//
-// $("#1").on('click', function() {
-// 	$(this).addClass("active-blue");
-// });
-//
-// $("#2").on('click', function() {
-// 	$(this).addClass("active-red");
-// });
-
-
-
+$(".thecard").on('click', function () {
+	$(this).addClass("flip");
+})
 }); // document doc ready
