96397f72d2dea248dade05d1489ce9b740524167
Josh <pana23@gmail.com>
2020-02-25 16:07:12 +1100

    Dry up the JS code

--- js/main.js
+++ js/main.js
@@ -52,35 +52,27 @@ for (let i = 0; i < 9; i++) {
 	});
 
 	}
-
+  const gameAlert = "<h4>You have won the game</h4>";
 	// Check if the player has won
 	// There is only 8 way a player can win
 	// (0-1-2) (3,4,5) (6,7,8) (0,3,6) (1,4,7) (2,5,8) (0,4,8) (2,4,6)
 	const isGameOver = function () {
 		if (boxValues[0] === boxValues[1] && boxValues[2] === boxValues[1] & boxValues[0] !== "") {
-			console.log("You have won");
-			$(".game-status").html("<h4>You have won the game</h4>");
+			$(".game-status").html(gameAlert);
 		} else if(boxValues[3] === boxValues[4] && boxValues[5] === boxValues[3] & boxValues[3] !== "") {
-			console.log("You have won");
-			$(".game-status").html("<h4>You have won the game</h4>");
+			$(".game-status").html(gameAlert);
 		} else if (boxValues[6] === boxValues[7] && boxValues[8] === boxValues[6] & boxValues[6] !== "") {
-			console.log("You have won");
-			$(".game-status").html("<h4>You have won the game</h4>");
+			$(".game-status").html(gameAlert);
 		} else if (boxValues[0] === boxValues[3] && boxValues[6] === boxValues[0] & boxValues[0] !== "") {
-			console.log("You have won");
-			$(".game-status").html("<h4>You have won the game</h4>");
+			$(".game-status").html(gameAlert);
 		} else if (boxValues[1] === boxValues[4] && boxValues[7] === boxValues[1] & boxValues[1] !== "") {
-			console.log("You have won");
-			$(".game-status").html("<h4>You have won the game</h4>");
+			$(".game-status").html(gameAlert);
 		} else if (boxValues[2] === boxValues[5] && boxValues[8] === boxValues[2] & boxValues[2] !== "") {
-			console.log("You have won");
-			$(".game-status").html("<h4>You have won the game</h4>");
+			$(".game-status").html(gameAlert);
 		} else if (boxValues[0] === boxValues[4] && boxValues[8] === boxValues[0] & boxValues[0] !== "") {
-			console.log("You have won");
-			$(".game-status").html("<h4>You have won the game</h4>");
+			$(".game-status").html(gameAlert);
 		} else if (boxValues[2] === boxValues[4] && boxValues[6] === boxValues[2] & boxValues[2] !== "") {
-			console.log("You have won");
-			$(".game-status").html("<h4>You have won the game</h4>");
+			$(".game-status").html(gameAlert);
 		} else if (boxValues[0] !== "" && // Finding if the game is a draw
 		           boxValues[1] !== "" &&
 							 boxValues[2] !== "" &&
